a70fdc11be9bb54a18c82c86892742012a614fb7
James Taylor <james.taylor5@myport.ac.uk>
2014-03-03 21:08:54 +0000

    Added images to frontend
    Added images to the frontend

--- MouseControl/obj/x86/Release/MouseControl_Content.g.i.cs
+++ MouseControl/obj/x86/Release/MouseControl_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("mousecontrolicon.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("navigate.png")]
 
 
